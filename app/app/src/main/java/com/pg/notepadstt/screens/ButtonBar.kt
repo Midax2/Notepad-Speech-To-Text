30d45583905adbfcfc874e8f96f543625841a96e
Torba2207 <alexnychyporchuk@gmail.com>
2025-05-15 20:33:05 +0200

    feat: Record button functionality

--- app/app/src/main/java/com/pg/notepadstt/screens/ButtonBar.kt
+++ app/app/src/main/java/com/pg/notepadstt/screens/ButtonBar.kt
@@ -1,5 +1,6 @@
 package com.pg.notepadstt.screens
 
+import android.util.Log
 import android.widget.Button
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -8,11 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -27,6 +31,7 @@ fun ButtonBar(
     sttProcessor: SpeechToTextProcessor,
     recorder: AudioRecorder
 ){
+    val textFromSpeech= remember { mutableStateOf("") }
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -39,8 +44,16 @@ fun ButtonBar(
             name = "Save"
         )
         BottomBarButton(
-            iconInt = R.drawable.mic,
-            onClickEvent = {  },
+            iconInt = if (!recorder.isRecording.value) R.drawable.mic else null,
+            iconVector = if (recorder.isRecording.value) Icons.Default.AddCircle else null,
+            onClickEvent = {
+                if (recorder.isRecording.value) {
+                    recorder.stopRecording()
+                    textFromSpeech.value=sttProcessor.runInference("temp_audio.wav")
+                    Log.i("Result Text from Speech:","Content:\" ${textFromSpeech.value}\"")
+                }
+                else recorder.startRecording()
+            },
             name = "Record"
         )
 
